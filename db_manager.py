708889b54cb59ef528d0238bfa4f87a677ede121
Juha Siivikko <juha@siivikko.fi>
2022-01-07 17:00:49 +0200

    DB functionality added

---
+++ db_manager.py
@@ -0,0 +1,58 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm.exc import NoResultFound
+from db_model import Transactions
+import time
+import uuid
+
+class DbManager:
+
+  def __init__(self, echo_mode=False):
+    Base = declarative_base()
+    engine = create_engine('sqlite:///wallet_service_db', echo=echo_mode)
+    Base.metadata.bind = engine
+    self.session = sessionmaker(bind=engine)()
+ 
+  def __del__(self):
+    self.session.close()
+  
+  def __exit__(self, *err):
+    pass
+  
+  def __enter__(self):
+    return self
+
+  def close_session(self):
+    self.session.close()    
+
+  def insert_transaction(self, address, amount, wallet_id, tx_size):
+    unsent = self.get_unsent()
+    if unsent:
+      internal_txid = unsent[0].internal_txid
+    else:
+      internal_txid = str(uuid.uuid4().hex)
+
+    # Only sent transactions have txid and timestamp
+    obj = Transactions(
+        internal_txid = internal_txid,
+        txid = None,
+        address = address,
+        amount = amount,
+        wallet_id = wallet_id,
+        tx_size = tx_size,
+        timestamp_ms = None
+      )
+    self.session.add(obj)
+    self.session.commit()
+    return obj, unsent
+
+  def get_unsent(self):
+    return self.session.query(Transactions).filter(Transactions.txid == None).all() 
+
+  def update_transaction(self, internal_txid, txid):
+    objs = self.session.query(Transactions).filter(Transactions.internal_txid == internal_txid).all()
+    for obj in objs:
+      obj.txid = txid
+      obj.timestamp_ms = int(time.time())
+    self.session.commit()
---
+++ db_model.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy import String, Column, BigInteger, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///wallet_service_db')
+
+Base = declarative_base()
+
+class Transactions(Base):
+    __tablename__ = 'transactions'
+    internal_txid = Column(String(250), primary_key = True)
+    txid = Column(String(250))
+    address = Column(String(250), primary_key = True)
+    amount = Column(Integer)
+    wallet_id = Column(Integer)
+    tx_size = Column(Integer)
+    timestamp_ms = Column(BigInteger)
+
+Base.metadata.create_all(engine)
