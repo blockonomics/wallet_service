8762330c61bfccfc68da0eaf381953314face29f
Juha Siivikko <juha@siivikko.fi>
2023-01-30 22:33:05 +0200

    Dont create a new network connection

--- electrum_cmd_util.py
+++ electrum_cmd_util.py
@@ -310,7 +310,8 @@ class APICmdUtil:
     ''' Get wallet balance
         Syncs wallet to network and returns both confirmed and unconfirmed amounts
     '''
-    self.cmd_manager.wait_for_wallet_sync(self.cmd_manager.wallet, True)
+    while not self.cmd_manager.wallet.is_up_to_date():
+      time.sleep(1)
     balance = self.cmd_manager.get_balance(self.cmd_manager.wallet)
     confirmed = balance[0]
     unconfirmed = balance[1]
